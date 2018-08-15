a326cceff4d99eca30b894399a47e37c0d715b98
Magno-D <40536442+Magno-D@users.noreply.github.com>
2018-08-15 17:00:45 +0300

    Update mk.js

--- mk.js
+++ mk.js
@@ -419,5 +419,23 @@ function shuffle(queue) {
 /*---------------------*/
 /* MISC FUNCTIONS END */
 /*-------------------*/
-
+const developers = ["331975722283302912"]
+const adminprefix = "=";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
 bot.login(process.env.BOT_TOKEN);
